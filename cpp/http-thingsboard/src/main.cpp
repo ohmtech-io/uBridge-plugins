2c502dc32e6a65ff9d171479bebee854645044bf
Daniel Mancuso <damancuso@gmail.com>
2025-01-18 12:48:58 -0300

    working on THingsboard HTTP integration

---
+++ cpp/http-thingsboard/src/main.cpp
@@ -0,0 +1,183 @@
+/***************************************************************************
+*** MIT License ***
+*
+*** Copyright (c) 2020-2025 Daniel Mancuso - OhmTech.io **
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+****************************************************************************/
+#include <fstream>
+#include <iostream>
+#include <chrono>
+
+#include <cxxopts.hpp>
+
+#include "uBridgeConfig.h"
+#include "ubridgeClient.h"
+#include "thingsboard.h"
+#include <curl/curl.h>
+
+// for convenience
+using json = nlohmann::json;
+
+ubridge::Config cfg;
+ThingsboardClient* pThingsboardClient;
+
+void subsMessageHandler(ubridge::message& message) {
+	LOG_S(9) << "cb msg: " << message.topic << message.data ;
+
+	std::string deviceId = message.topic;
+	
+	std::string prefix = "/sensors/";
+	std::string::size_type i = deviceId.find(prefix);
+
+	if (i != std::string::npos){
+		deviceId.erase(i, prefix.length());
+ 	}
+
+ 	LOG_S(9) << "deviceId: " << deviceId;
+	pThingsboardClient->sendTelemetry(deviceId, message.data);
+}
+
+json loadConfigFile(std::string& config_file){
+	json jconfig;
+
+	try {
+		LOG_S(INFO) << "Opening " << config_file;
+		// read in the json file
+		std::ifstream file(config_file, std::ifstream::in);
+
+		if (not file.is_open()) {
+			LOG_S(ERROR) << "Error, couldn't open configuration file " << config_file;
+			exit(1);
+		}
+		// parse using "ignore_comments=true" so we can use comments on the config file
+		jconfig = json::parse(file, nullptr, true, true); 
+
+	} catch (const std::runtime_error& ex) {
+		LOG_S(WARNING) << "Error parsing options: " << ex.what();
+	} catch (const nlohmann::detail::parse_error& ex) {
+		LOG_S(WARNING) << "Error parsing json: " << ex.what();
+	}
+
+	LOG_S(INFO) << "Loaded configuration: "<< std::setw(2) << jconfig;
+
+	return jconfig;
+}
+
+ubridge::Config loadBridgeConfig(json& jconfig) {
+	ubridge::Config config;
+	//override defaults on uBridgeConfig.h if they are present on the json file
+	if (jconfig.contains("configSockUrl")) {
+		config.configSockUrl = jconfig.at("configSockUrl");
+		LOG_S(7) << "uBridge ReqRepSocket url: " << config.configSockUrl;
+	}
+	if (jconfig.contains("streamSockUrl")) {
+		config.streamSockUrl = jconfig.at("streamSockUrl");
+		LOG_S(7) << "uBridge Stream Socket url: " << config.streamSockUrl;
+	}
+	return config;
+}
+
+thingsboardConfig_t loadThingsboardConfig(json& jconfig) {
+	thingsboardConfig_t config;
+	
+	if (jconfig.contains("serverUrl")) {
+		config.serverUrl = jconfig.at("serverUrl");
+		LOG_S(7) << "ThingsBoard server URL: " << config.serverUrl;
+	}
+	if (jconfig.contains("accessToken")) {
+		config.accessToken = jconfig.at("accessToken");
+		LOG_S(7) << "Using access token: " << config.accessToken;
+	}
+
+	return config;
+}
+
+int main(int argc, char *argv[])
+{
+	cxxopts::Options options("ThingsboardPlugin", "This plugin app. acts as a client of ubridge, \
+			subscribing to data from the sensors and sending it to ThingsBoard via HTTP");
+
+    options.add_options()
+   		("c,config", "JSON configuration file name", cxxopts::value<std::string>()->default_value("/etc/ubridge/thingsboard-plugin-config.json"))
+        ("v,verbose", "Verbosity output level (0-9)", cxxopts::value<int>()->default_value("0"))
+        ("h,help", "Print usage")
+    ;
+
+    std::string config_file;
+
+	try {
+		auto result = options.parse(argc, argv);
+
+		if (result.count("help")) {
+	  		std::cout << options.help() << std::endl;
+	  		exit(0);
+		}
+
+		if (result.count("verbose")) {
+			loguru::g_stderr_verbosity = result["verbose"].as<int>();
+		}
+
+		/* if no config provided with -c, use default on /etc/ubridge */
+		config_file = result["config"].as<std::string>();	
+	}
+	catch (const cxxopts::OptionException& e) {
+		std::cout << "error parsing options: " << e.what() << std::endl;
+		exit(1);
+	}
+
+	loguru::init(argc, argv);
+	// Only log INFO, WARNING, ERROR and FATAL
+	loguru::add_file("/tmp/ubridge-thingsboard.log", loguru::Truncate, loguru::Verbosity_INFO);
+
+	LOG_S(INFO) << "--- Initializing ** ThingsBoard HTTP plugin **... ---";
+
+	//Load the json file from disk...
+	json jconfig = loadConfigFile(config_file);
+
+	//parse the uBridge config:
+	ubridge::Config bridgeConfig = loadBridgeConfig(jconfig["ubridge"]);
+	//parse the ThingsBoard config:
+	thingsboardConfig_t tbConfig = loadThingsboardConfig(jconfig["thingsboard"]);
+
+	// Initialize CURL for HTTP requests
+	curl_global_init(CURL_GLOBAL_ALL);
+	
+	ThingsboardClient thingsboardClient{tbConfig};
+	pThingsboardClient = &thingsboardClient;
+
+	ReqRepClient uBridgeClient{bridgeConfig.configSockUrl, bridgeConfig.streamSockUrl.c_str()};	
+	
+	/* check uBridge side: */	
+	LOG_S(INFO) << "Pinging uBridge server..." ;
+	if (uBridgeClient.connect() != 0) {
+		/* wait for ubridge config server */
+		LOG_S(INFO) << "uBridge Server not found!" ;	
+		LOG_S(INFO) << "Waiting for server..." ;	
+		while (uBridgeClient.connect() !=0) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+		}
+	}
+
+	LOG_S(INFO) << "Start sending data to ThingsBoard at " << tbConfig.serverUrl;
+	//start message receiving loop...
+	uBridgeClient.subscribe("/sensors", subsMessageHandler); //subscribe to all sensors
+	
+	return 0;
+}//main
---
+++ cpp/http-thingsboard/src/thingsboard.h
@@ -0,0 +1,104 @@
+/***************************************************************************
+*** MIT License ***
+*
+*** Copyright (c) 2020-2025 Daniel Mancuso - OhmTech.io **
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+****************************************************************************/
+
+#pragma once
+
+#include <string>
+#include <curl/curl.h>
+#include <nlohmann/json.hpp>
+#include "loguru.hpp"
+
+struct thingsboardConfig_t {
+    std::string serverUrl = "http://localhost:8080";  // Default ThingsBoard server URL
+    std::string accessToken = "";                     // Device access token
+};
+
+class ThingsboardClient {
+private:
+    thingsboardConfig_t config;
+    CURL* curl;
+
+    // Callback function to handle CURL response
+    static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
+        ((std::string*)userp)->append((char*)contents, size * nmemb);
+        return size * nmemb;
+    }
+
+public:
+    ThingsboardClient(const thingsboardConfig_t& cfg) : config(cfg) {
+        curl = curl_easy_init();
+        if (!curl) {
+            LOG_S(ERROR) << "Failed to initialize CURL";
+            throw std::runtime_error("CURL initialization failed");
+        }
+    }
+
+    ~ThingsboardClient() {
+        if (curl) {
+            curl_easy_cleanup(curl);
+        }
+    }
+
+    bool sendTelemetry(const std::string& deviceId, const std::string& jsonData) {
+        if (!curl) {
+            LOG_S(ERROR) << "CURL not initialized";
+            return false;
+        }
+
+        // Construct the URL for the device
+        std::string url = config.serverUrl + "/api/v1/" + config.accessToken + "/telemetry";
+
+        // Set up headers
+        struct curl_slist* headers = NULL;
+        headers = curl_slist_append(headers, "Content-Type: application/json");
+
+        std::string response;
+
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, jsonData.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+        // Perform the request
+        CURLcode res = curl_easy_perform(curl);
+        curl_slist_free_all(headers);
+
+        if (res != CURLE_OK) {
+            LOG_S(ERROR) << "Failed to send telemetry: " << curl_easy_strerror(res);
+            return false;
+        }
+
+        long http_code = 0;
+        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+
+        if (http_code != 200) {
+            LOG_S(ERROR) << "HTTP error: " << http_code << ", Response: " << response;
+            return false;
+        }
+
+        LOG_S(9) << "Successfully sent telemetry for device " << deviceId;
+        return true;
+    }
+}; 
